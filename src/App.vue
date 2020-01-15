e4c1edaec84478ba7f11cbe0d11d56a620f54ab3
danillm <danillo@mitrabi.com>
2020-01-15 00:26:37 -0300

    ,

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-container fluid class="container">
+      <v-card class="card">
+        <img src="./assets/hqdefault.jpg" />
+        <div class="content" style="height: 45%; width: 100%;">
+          <v-progress-linear indeterminate color="green darken-2"></v-progress-linear>
+          <div>
+            <h5>Memória</h5>
+          <v-simple-table dense>
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">Total</th>
+                  <th class="text-left">Em uso</th>
+                  <th class="text-left">Disponível</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, idx) in ramStatus" :key="idx">
+                  <td>{{ item.all + 'GB' }}</td>
+                  <td>{{ item.used }}</td>
+                  <td>{{ item.free }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+          </div>
+        </div>
+        <div style="height: 5%" class="btn-box">
+          <v-btn small color="yellow">reiniciar</v-btn>
+          <v-btn small color="red">derrubar</v-btn>
+          <v-btn small color="green">subir</v-btn>
+        </div>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    ramStatus: [{ all: 15, used: 7, free: 8 }]
+    //
+  })
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+  background-color: lightslategray;
+  widows: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.card {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  padding: 10px;
+}
+.btn-box {
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+}
+.content{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around
 }
 </style>
